e1516dd18e4fd6a10fd6da43d23975a732c01bde
leyvsi <sylviecosta78500@gmail.com>
2026-01-24 00:58:48 +0000

    add task 2 and test 2

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that prints a full name in the format:
+"My name is <first_name> <last_name>"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints 'My name is <first_name> <last_name>'.
+
+    Raises TypeError if first_name or last_name is not a string.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}".strip())
