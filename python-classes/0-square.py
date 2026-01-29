6db6aa8440b2a4b0760fe9f4817efe4ab8a23c2d
leyvsi <sylviecosta78500@gmail.com>
2026-01-29 14:17:17 +0000

    add doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-class Square:
-   """Class Square that defines a square object
+   """This module defines an empty class Square.
     """
-     def__init__(self, size)
-     self.__size = size
+class Square:
+ """Represents an empty square."""
+pass
