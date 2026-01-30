5f9dc18efb45ea0c974e40821e5a6818c469dc97
leyvsi <sylviecosta78500@gmail.com>
2026-01-30 21:59:47 +0100

    add task 0-9 one shoot

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Defines a rectangle"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height"""
+        return self.__height
+
+    @height.set
