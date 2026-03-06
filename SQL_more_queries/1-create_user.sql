ec8460a21bf6daeeff43676c62f85c1019288a7c
leyvsi <sylviecosta78500@gmail.com>
2026-03-06 13:38:22 +0100

    fix task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- Create the MySQL server user user_0d_1
+-- Creates the user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on your MySQL server to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
