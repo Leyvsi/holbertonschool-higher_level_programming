bd4a492125dbdb80bd789aa535827230f2633ca2
leyvsi <sylviecosta78500@gmail.com>
2026-03-06 13:16:02 +0100

    add task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on your MySQL server to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
