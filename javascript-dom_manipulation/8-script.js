d0b9d0638167832898de8943cd41715b01d79097
leyvsi <sylviecosta78500@gmail.com>
2026-03-27 11:25:41 +0100

    add task 0-8-script.js

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const idElement = document.getElementById('hello');
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(data => {
+      idElement.textContent = data.hello;
+    });
+});
